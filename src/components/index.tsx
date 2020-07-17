b9893d762ff61bf558d23627bd9c76f38c37e40e
Jaime Gensler <jaimegensler0@gmail.com>
2020-07-17 11:49:05 -0700

    Add App comp

---
+++ src/components/index.tsx
@@ -0,0 +1,13 @@
+import FOLInput from './FOLInput';
+
+export default function App() {
+    return (
+        <>
+            <header>Hello there</header>
+            <main>
+                <FOLInput />
+                <FOLInput />
+            </main>
+        </>
+    );
+}
